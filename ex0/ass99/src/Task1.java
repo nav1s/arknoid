caad1f87225ce90b3e83b8db7485e12e31a3aab8
root <root@localhost>
2024-05-08 09:43:42 +0300

    add new example files from github

---
+++ ex0/ass99/src/Task1.java
@@ -0,0 +1,13 @@
+
+/**
+ * The main class for Task1.
+ */
+public class Task1 {
+   /** The main method.
+    *
+    * @param args ignored
+    */
+   public static void main(String[] args) {
+      System.out.println("Hello Task1");
+   }
+}
---
+++ ex0/ass99/src/Task2.java
@@ -0,0 +1,13 @@
+
+/**
+ * The main class for task 2.
+ */
+public class Task2 {
+   /** The main method.
+    *
+    * @param args ignored
+    */
+   public static void main(String[] args) {
+      System.out.println("Hello Task2");
+   }
+}
