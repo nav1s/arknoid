8ee98b1d2444c567954ad97918caa458478b2a75
root <root@localhost>
2024-05-11 08:22:00 +0300

    add working code for SumOfInteger

--- ex1/src/SumOfInteger.java
+++ ex1/src/SumOfInteger.java
@@ -4,15 +4,29 @@
  * The main class for SumOfInteger.
  */
 public class SumOfInteger {
-   /** The main method.
+   /**
+    * The main method.
     *
     * @param args ignored
     */
    public static void main(String[] args) {
-      if (args.length == 0) {
+      // exit if the user didn't give us a single arg
+      if (args.length != 1) {
          System.out.println("Invalid input");
          System.exit(0);
       }
-      System.out.println(args[0].charAt(0));
+      // save our number for ease of use
+      String numToFindSumOf = args[0];
+      int numSum = 0;
+
+      // loop over the number we got
+      for (int i = 0; i < numToFindSumOf.length(); i++) {
+         int digit = Character.getNumericValue(numToFindSumOf.charAt(i));
+         numSum += digit;
+      }
+
+      // print the sum of the number
+      System.out.println(numSum);
+
    }
 }
