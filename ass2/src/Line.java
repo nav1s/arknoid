3d48b2979849b91ca83d904abc8092d41f89eed1
root <root@localhost>
2024-05-20 14:33:51 +0300

    fix Line set both start and end to this.start

--- ass2/src/Line.java
+++ ass2/src/Line.java
@@ -24,7 +24,7 @@ public class Line {
      */
     public Line(double x1, double y1, double x2, double y2) {
         this.start = new Point(x1, y1);
-        this.start = new Point(x2, y2);
+        this.end = new Point(x2, y2);
     }
 
     /**
