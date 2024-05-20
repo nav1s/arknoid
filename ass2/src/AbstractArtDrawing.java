73bfbe7a245d03a144e81a78f8532bd0282aa19d
root <root@localhost>
2024-05-20 14:34:17 +0300

    implement some helper funcs in AbstractArtDrawing

--- ass2/src/AbstractArtDrawing.java
+++ ass2/src/AbstractArtDrawing.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.util.Random;
 
@@ -22,37 +23,35 @@ public class AbstractArtDrawing {
         return new Line(x1, x2, y1, y2);
     }
 
-    // public void drawLine(Line l, DrawSurface d) {
-    //     DrawSurface d = gui.getDrawSurface();
+    /**
+     * @param line
+     * @param drawer
+     */
+    public void drawLine(Line line, DrawSurface drawer) {
+        int x1 = (int) line.start().getX();
+        int x2 = (int) line.end().getX();
 
-    // }
+        int y1 = (int) line.start().getY();
+        int y2 = (int) line.end().getY();
+
+        drawer.drawLine(x1, x2, y1, y2);
+    }
 
     /**
      */
     public void drawRandomLines() {
-        Random rand = new Random(); // create a random-number generator
         // Create a window with the title "Random Circles Example"
         // which is 400 pixels wide and 300 pixels high.
         GUI gui = new GUI("abc", 400, 300);
-        DrawSurface d = gui.getDrawSurface();
+        DrawSurface drawer = gui.getDrawSurface();
         for (int i = 0; i < 10; ++i) {
-            int x1 = rand.nextInt(400) + 1; // get integer in range 1-400
-            int x2 = rand.nextInt(400) + 1; // get integer in range 1-400
-            int y1 = rand.nextInt(300) + 1; // get integer in range 1-300
-            int y2 = rand.nextInt(300) + 1; // get integer in range 1-300
+            Line line = generateRandomLine();
 
-            d.setColor(Color.RED);
-            d.drawLine(x1, x2, y1, y2);
+            drawer.setColor(Color.RED);
+            drawLine(line, drawer);
         }
 
-        int x1 = 0;
-        int x2 = rand.nextInt(400) + 1; // get integer in range 1-400
-        int y1 = rand.nextInt(300) + 1; // get integer in range 1-300
-        int y2 = rand.nextInt(300) + 1; // get integer in range 1-300
-
-        d.setColor(Color.RED);
-        d.drawLine(x1, x2, y1, y2);
-        gui.show(d);
+        gui.show(drawer);
     }
 
     /**
