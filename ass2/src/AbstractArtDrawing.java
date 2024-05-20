1ac53ac43d31d575cd78be109a1d6d3fa6efc60e
root <root@localhost>
2024-05-20 14:12:59 +0300

    add some initial stuff to AbstractArtDrawing

--- ass2/src/AbstractArtDrawing.java
+++ ass2/src/AbstractArtDrawing.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.util.Random;
 
 import biuoop.DrawSurface;
@@ -9,20 +10,48 @@ import biuoop.GUI;
  */
 public class AbstractArtDrawing {
     /**
+     * @return A randomly generated line
      */
-    public void drawRandomCircles() {
+    public Line generateRandomLine() {
+        Random rand = new Random(); // create a random-number generator
+        int x1 = rand.nextInt(400) + 1; // get integer in range 1-400
+        int x2 = rand.nextInt(400) + 1; // get integer in range 1-400
+        int y1 = rand.nextInt(300) + 1; // get integer in range 1-300
+        int y2 = rand.nextInt(300) + 1; // get integer in range 1-300
+
+        return new Line(x1, x2, y1, y2);
+    }
+
+    // public void drawLine(Line l, DrawSurface d) {
+    //     DrawSurface d = gui.getDrawSurface();
+
+    // }
+
+    /**
+     */
+    public void drawRandomLines() {
         Random rand = new Random(); // create a random-number generator
         // Create a window with the title "Random Circles Example"
         // which is 400 pixels wide and 300 pixels high.
-        GUI gui = new GUI("Random Circles Example", 400, 300);
+        GUI gui = new GUI("abc", 400, 300);
         DrawSurface d = gui.getDrawSurface();
         for (int i = 0; i < 10; ++i) {
-            int x = rand.nextInt(400) + 1; // get integer in range 1-400
-            int y = rand.nextInt(300) + 1; // get integer in range 1-300
-            int r = 5 * (rand.nextInt(4) + 1); // get integer in 5,10,15,20
+            int x1 = rand.nextInt(400) + 1; // get integer in range 1-400
+            int x2 = rand.nextInt(400) + 1; // get integer in range 1-400
+            int y1 = rand.nextInt(300) + 1; // get integer in range 1-300
+            int y2 = rand.nextInt(300) + 1; // get integer in range 1-300
+
             d.setColor(Color.RED);
-            d.fillCircle(x, y, r);
+            d.drawLine(x1, x2, y1, y2);
         }
+
+        int x1 = 0;
+        int x2 = rand.nextInt(400) + 1; // get integer in range 1-400
+        int y1 = rand.nextInt(300) + 1; // get integer in range 1-300
+        int y2 = rand.nextInt(300) + 1; // get integer in range 1-300
+
+        d.setColor(Color.RED);
+        d.drawLine(x1, x2, y1, y2);
         gui.show(d);
     }
 
@@ -30,8 +59,8 @@ public class AbstractArtDrawing {
      * @param args ignored.
      */
     public static void main(String[] args) {
-        SimpleGuiExample example = new SimpleGuiExample();
-        example.drawRandomCircles();
+        AbstractArtDrawing example = new AbstractArtDrawing();
+        example.drawRandomLines();
     }
-    
+
 }
