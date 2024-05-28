1af9489c5f7f6dc47619efe4af9262faf07d57b2
root <root@localhost>
2024-05-28 09:38:01 +0300

    add buggy implementation of interesctionWith

--- ass2/src/Line.java
+++ ass2/src/Line.java
@@ -1,6 +1,6 @@
 
 /**
- * This class does some simple testing of the Point and Line classes.
+ * This class represents a line in a two dimensional cartesian plane.
  *
  */
 public class Line {
@@ -9,7 +9,8 @@ public class Line {
 
     /**
      * @param start the starting point of our line
-     * @param end the ending point of our line
+     * @param end   the ending point of our line
+     *              taken from the exercise description.
      */
     public Line(Point start, Point end) {
         this.start = start;
@@ -17,10 +18,11 @@ public class Line {
     }
 
     /**
-     * @param x1 coordinate x of the start line
-     * @param y1
-     * @param x2
-     * @param y2
+     * @param x1 coordinate x of the starting point of the line
+     * @param y1 coordinate y of the starting point of the line
+     * @param x2 coordinate x of the ending point of the line
+     * @param y2 coordinate y of the ending point of the line
+     *           taken from the exercise description.
      */
     public Line(double x1, double y1, double x2, double y2) {
         this.start = new Point(x1, y1);
@@ -29,6 +31,7 @@ public class Line {
 
     /**
      * @return length of the line
+     *         taken from the exercise description.
      */
     public double length() {
         return this.start.distance(this.end);
@@ -36,6 +39,7 @@ public class Line {
 
     /**
      * @return middle point of the line
+     *         taken from the exercise description.
      */
     public Point middle() {
         double midX = (this.start.getX() + this.end.getX()) / 2;
@@ -45,6 +49,7 @@ public class Line {
 
     /**
      * @return start point of the line
+     *         taken from the exercise description.
      */
     public Point start() {
         return this.start;
@@ -52,43 +57,84 @@ public class Line {
 
     /**
      * @return end point of the line
+     *         taken from the exercise description.
      */
     public Point end() {
         return this.end;
     }
 
     /**
-     * @param other
+     * @param other the line we are comparing to
      * @return true if the lines intersect, false otherwise
+     *         taken from the exercise description.
      */
     public boolean isIntersecting(Line other) {
         return true;
     }
 
     /**
-     * @param other1
-     * @param other2
+     * @param other1 the first line we are comparing to
+     * @param other2 the second line we are comparing to
      * @return true if this 2 lines intersect with this line, false otherwise
+     *         taken from the exercise description.
      */
     public boolean isIntersecting(Line other1, Line other2) {
-        return true;
+        return isIntersecting(other1) && isIntersecting(other2);
     }
 
     /**
-     * @param other
+     * @param other the line we are comparing to
      * @return Returns the intersection point if the lines intersect, and null
      *         otherwise
+     *         taken from the exercise description.
      */
     public Point intersectionWith(Line other) {
-        return new Point(0, 0);
+        double x1 = this.start.getX();
+        double y1 = this.start.getY();
+
+        double x2 = this.end.getX();
+        double y2 = this.end.getY();
+
+        double x3 = other.start.getX();
+        double y3 = other.start.getY();
+
+        double x4 = other.end.getX();
+        double y4 = other.end.getY();
+
+        double a = ((x4 - x3) * (y3 - y1) - (y4 - y3) * (x3 - x1));
+        double b = ((x4 - x3) * (y2 - y1) - (y4 - y3) * (x2 - x1));
+        double c = ((x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1));
+        double d = ((x4 - x3) * (y2 - y1) - (y4 - y3) * (x2 - x1));
+        System.out.println("a:" + a);
+        System.out.println("b:" + b);
+        System.out.println("c:" + c);
+        System.out.println("d:" + d);
+
+        if (b == 0 || d == 0) {
+            return null;
+        }
+
+        double alpha = a / b;
+        double beta = c / d;
+        System.out.println("alpha:" + alpha);
+        System.out.println("beta:" + beta);
+        if (alpha >= 0 && alpha <= 1 && beta >= 0 && beta <= 1) {
+            double x0 = x1 + alpha * (x2 - x1);
+            double y0 = y1 + beta * (y2 - y1);
+            return new Point(x0, y0);
+        }
+
+        return null;
     }
 
     /**
      * @param other the line we want to compare to
      * @return true is the lines are equal, false otherwise
+     *         taken from the exercise description.
      */
     public boolean equals(Line other) {
-        // if both lines have same start and same end then we can conclude they are equal
+        // if both lines have same start and same end then we can conclude they are
+        // equal
         if (this.start.equals(other.start) && this.end.equals(other.end)) {
             return true;
         }
@@ -98,12 +144,9 @@ public class Line {
         return false;
     }
 
-    /**
-     * @return A string
-     */
     @Override
     public String toString() {
-        return "";
+        return "Line [start=" + start + ", end=" + end + "]";
     }
 
 }
