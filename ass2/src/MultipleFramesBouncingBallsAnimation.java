7345b00b45cd104fbc5bb00089b365aa6d2b7ab4
root <root@localhost>
2024-05-30 14:24:39 +0300

    implement balls being hidden when beneath yellow rectangle

--- ass2/src/MultipleFramesBouncingBallsAnimation.java
+++ ass2/src/MultipleFramesBouncingBallsAnimation.java
@@ -23,36 +23,44 @@ public class MultipleFramesBouncingBallsAnimation {
     private static final int YELLOW_RECTANGLE_WIDTH = 150;
 
     /**
-     * @param sizes
+     * @param sizes an array containing the size of our radiuses
      */
     private void drawAnimation(int[] sizes) {
+        // setup some initial variables
         Sleeper sleeper = new Sleeper();
         GUI gui = new GUI(GUI_TITLE, GUI_HEIGHT, GUI_WIDTH);
         Ball[] balls = new Ball[sizes.length];
 
+        // generate an array of balls
         for (int i = 0; i < sizes.length; i++) {
             int minHeight = GRAY_RECTANGLE_Y + sizes[i];
-            int maxHeight = YELLOW_RECTANGLE_Y - sizes[i];
+            int maxHeight = GRAY_RECTANGLE_Y + GRAY_RECTANGLE_HEIGHT - sizes[i];
 
             int minWidth = GRAY_RECTANGLE_X + sizes[i];
-            int maxWidth = YELLOW_RECTANGLE_X - sizes[i];
+            int maxWidth = GRAY_RECTANGLE_X + GRAY_RECTANGLE_WIDTH - sizes[i];
 
             balls[i] = new Ball(sizes[i], minHeight, maxHeight, minWidth, maxWidth);
         }
 
+        // main loop
         while (true) {
             DrawSurface surface = gui.getDrawSurface();
+
+            // create the gray rectangle
             surface.setColor(java.awt.Color.GRAY);
             surface.fillRectangle(GRAY_RECTANGLE_X, GRAY_RECTANGLE_Y, GRAY_RECTANGLE_WIDTH, GRAY_RECTANGLE_HEIGHT);
 
-            surface.setColor(java.awt.Color.YELLOW);
-            surface.fillRectangle(YELLOW_RECTANGLE_X, YELLOW_RECTANGLE_Y, YELLOW_RECTANGLE_WIDTH,
-                    YELLOW_RECTANGLE_HEIGHT);
-
+            // move our balls
             for (int i = 0; i < balls.length; i++) {
                 balls[i].moveOneStep();
                 balls[i].drawOn(surface);
             }
+
+            // create the yellow rectangle
+            surface.setColor(java.awt.Color.YELLOW);
+            surface.fillRectangle(YELLOW_RECTANGLE_X, YELLOW_RECTANGLE_Y, YELLOW_RECTANGLE_WIDTH,
+                    YELLOW_RECTANGLE_HEIGHT);
+
 
             gui.show(surface);
             sleeper.sleepFor(NUMBER_OF_MILLISECONDS_TO_WAIT);
