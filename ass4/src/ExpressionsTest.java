31c625ec08050d9f3a3af46f39c44709e8b43e76
root <root@localhost>
2024-06-28 15:43:33 +0300

    add more simplify tests

--- ass4/src/ExpressionsTest.java
+++ ass4/src/ExpressionsTest.java
@@ -1,6 +1,9 @@
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * This class tests a few expressions
+ * This class tests a few expressions.
  */
 public class ExpressionsTest {
 
@@ -11,11 +14,24 @@ public class ExpressionsTest {
         Expression e = new And(new Xor(new Var("x"), new Var("y")), new Var("z"));
         System.out.println(e);
 
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("x", true);
+        map.put("y", true);
+        map.put("z", false);
+        // todo fix this
+        map.put("k", false);
+        try {
+            String output = e.evaluate(map).toString();
+            System.out.println(output);
+
+        } catch (Exception ignored) {
+            System.out.println("Error. Exception thrown during evaluation.");
+        }
+
         System.out.println(e.nandify());
         System.out.println(e.norify());
         System.out.println(e.simplify());
 
-        
     }
-    
+
 }
